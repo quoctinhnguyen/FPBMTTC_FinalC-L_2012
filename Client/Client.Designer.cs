b5c4be40137dd8ca5f118d56bd4eb33258867ba4
Kiệt Nguyễn Tuấn <tuankiet0996@outlook.com>
2017-05-28 09:32:48 +0700

    first

--- Client/Client.Designer.cs
+++ Client/Client.Designer.cs
@@ -33,12 +33,12 @@
             this.btnCONNECT = new System.Windows.Forms.Button();
             this.lblSTATUS = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnREQUEST = new System.Windows.Forms.Button();
             this.btnREVOKE = new System.Windows.Forms.Button();
             this.btnSEARCH = new System.Windows.Forms.Button();
@@ -50,26 +50,29 @@
             // 
             this.lblIP.AutoSize = true;
             this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIP.Location = new System.Drawing.Point(13, 13);
+            this.lblIP.Location = new System.Drawing.Point(17, 16);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIP.Name = "lblIP";
-            this.lblIP.Size = new System.Drawing.Size(20, 15);
+            this.lblIP.Size = new System.Drawing.Size(23, 18);
             this.lblIP.TabIndex = 0;
             this.lblIP.Text = "IP";
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(49, 12);
+            this.txtIP.Location = new System.Drawing.Point(65, 15);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIP.Multiline = true;
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(112, 21);
+            this.txtIP.Size = new System.Drawing.Size(148, 25);
             this.txtIP.TabIndex = 1;
             // 
             // btnCONNECT
             // 
             this.btnCONNECT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCONNECT.Location = new System.Drawing.Point(189, 12);
+            this.btnCONNECT.Location = new System.Drawing.Point(247, 15);
+            this.btnCONNECT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCONNECT.Name = "btnCONNECT";
-            this.btnCONNECT.Size = new System.Drawing.Size(88, 23);
+            this.btnCONNECT.Size = new System.Drawing.Size(117, 28);
             this.btnCONNECT.TabIndex = 2;
             this.btnCONNECT.Text = "CONNECT";
             this.btnCONNECT.UseVisualStyleBackColor = true;
@@ -78,9 +81,10 @@
             // 
             this.lblSTATUS.AutoSize = true;
             this.lblSTATUS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSTATUS.Location = new System.Drawing.Point(306, 13);
+            this.lblSTATUS.Location = new System.Drawing.Point(408, 16);
+            this.lblSTATUS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSTATUS.Name = "lblSTATUS";
-            this.lblSTATUS.Size = new System.Drawing.Size(59, 15);
+            this.lblSTATUS.Size = new System.Drawing.Size(72, 18);
             this.lblSTATUS.TabIndex = 3;
             this.lblSTATUS.Text = "STATUS";
             // 
@@ -93,70 +97,79 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(13, 44);
+            this.groupBox1.Location = new System.Drawing.Point(17, 54);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(405, 161);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(540, 198);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "INFORMATION REGISTER CA";
             // 
-            // label2
+            // textBox3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 30);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 15);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "COMPANY NAME";
+            this.textBox3.Location = new System.Drawing.Point(192, 124);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(276, 24);
+            this.textBox3.TabIndex = 4;
             // 
-            // label3
+            // textBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(6, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 15);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "EMAIL";
+            this.textBox2.Location = new System.Drawing.Point(192, 79);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 24);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 24);
+            this.textBox1.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 104);
+            this.label4.Location = new System.Drawing.Point(8, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.Size = new System.Drawing.Size(67, 18);
             this.label4.TabIndex = 2;
             this.label4.Text = "PHONE";
             // 
-            // textBox1
+            // label3
             // 
-            this.textBox1.Location = new System.Drawing.Point(144, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(208, 21);
-            this.textBox1.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "EMAIL";
             // 
-            // textBox2
+            // label2
             // 
-            this.textBox2.Location = new System.Drawing.Point(144, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(208, 21);
-            this.textBox2.TabIndex = 4;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(144, 101);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(208, 21);
-            this.textBox3.TabIndex = 4;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "COMPANY NAME";
             // 
             // btnREQUEST
             // 
             this.btnREQUEST.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnREQUEST.Location = new System.Drawing.Point(22, 232);
+            this.btnREQUEST.Location = new System.Drawing.Point(29, 286);
+            this.btnREQUEST.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnREQUEST.Name = "btnREQUEST";
-            this.btnREQUEST.Size = new System.Drawing.Size(92, 38);
+            this.btnREQUEST.Size = new System.Drawing.Size(123, 47);
             this.btnREQUEST.TabIndex = 5;
             this.btnREQUEST.Text = "REQUEST";
             this.btnREQUEST.UseVisualStyleBackColor = true;
@@ -164,9 +177,10 @@
             // btnREVOKE
             // 
             this.btnREVOKE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnREVOKE.Location = new System.Drawing.Point(123, 232);
+            this.btnREVOKE.Location = new System.Drawing.Point(164, 286);
+            this.btnREVOKE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnREVOKE.Name = "btnREVOKE";
-            this.btnREVOKE.Size = new System.Drawing.Size(92, 38);
+            this.btnREVOKE.Size = new System.Drawing.Size(123, 47);
             this.btnREVOKE.TabIndex = 5;
             this.btnREVOKE.Text = "REVOKE";
             this.btnREVOKE.UseVisualStyleBackColor = true;
@@ -174,9 +188,10 @@
             // btnSEARCH
             // 
             this.btnSEARCH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSEARCH.Location = new System.Drawing.Point(224, 232);
+            this.btnSEARCH.Location = new System.Drawing.Point(299, 286);
+            this.btnSEARCH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSEARCH.Name = "btnSEARCH";
-            this.btnSEARCH.Size = new System.Drawing.Size(92, 38);
+            this.btnSEARCH.Size = new System.Drawing.Size(123, 47);
             this.btnSEARCH.TabIndex = 5;
             this.btnSEARCH.Text = "SEARCH";
             this.btnSEARCH.UseVisualStyleBackColor = true;
@@ -184,18 +199,19 @@
             // btnVIEW
             // 
             this.btnVIEW.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVIEW.Location = new System.Drawing.Point(325, 232);
+            this.btnVIEW.Location = new System.Drawing.Point(433, 286);
+            this.btnVIEW.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVIEW.Name = "btnVIEW";
-            this.btnVIEW.Size = new System.Drawing.Size(92, 38);
+            this.btnVIEW.Size = new System.Drawing.Size(123, 47);
             this.btnVIEW.TabIndex = 5;
             this.btnVIEW.Text = "VIEW";
             this.btnVIEW.UseVisualStyleBackColor = true;
             // 
             // Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 285);
+            this.ClientSize = new System.Drawing.Size(573, 351);
             this.Controls.Add(this.btnVIEW);
             this.Controls.Add(this.btnSEARCH);
             this.Controls.Add(this.btnREVOKE);
@@ -205,6 +221,7 @@
             this.Controls.Add(this.btnCONNECT);
             this.Controls.Add(this.txtIP);
             this.Controls.Add(this.lblIP);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Client";
             this.Text = "Client";
             this.groupBox1.ResumeLayout(false);
